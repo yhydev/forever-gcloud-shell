c703a63c06b2356050e991336fe2f52b778a7ba9
yhydev <yhydev@outlook.com>
2021-11-21 13:07:08 +0000

    使用oauth

--- init.sh
+++ init.sh
@@ -1,4 +1,4 @@
 mkdir ~/.ssh
 ssh-keygen -f /root/.ssh/google_compute_engine -N ''
-echo $GCLOUD_ACCESS_TOKEN > gcloud_access_token
+curl -X POST   https://accounts.google.com/o/oauth2/token   -H 'content-type: application/x-www-form-urlencoded' -d "grant_type=refresh_token&client_id=$CLIENT_ID&client_secret=$CLIENT_SECRET&refresh_token=$REFRESH_TOKEN"|grep access|awk -F\" '{print $4}' > gcloud_access_token
 gcloud alpha cloud-shell ssh --command 'echo'  --access-token-file gcloud_access_token
